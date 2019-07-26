60cb97b9efb390d2c2e429e4ba708fce1457ee5e
Mohab Morsy <r2gm.ducky@gmail.com>
2019-07-26 13:24:36 +0200

    migrating to postgresql still

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ CLIENT_SECRET_FILE = 'client_secret.json'
 
 # End of login process
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "'postgres://gsdyhfpzvhxvbz:3903f4b5f0dbed10fe05b20fd63cc77c0fc7a7001a3cf1625204628c4f982693@ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/ddvjg9iiu4vo5r"
-DBSession = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://gsdyhfpzvhxvbz:3903f4b5f0dbed10fe05b20fd63cc77c0fc7a7001a3cf1625204628c4f982693@ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/ddvjg9iiu4vo5r"
+db = SQLAlchemy(app)
 
 # Connect to Database and create database session
-# engine = create_engine('sqlite:///itemscatalog.db?check_same_thread=False')
-# Base.metadata.bind = engine
-#
-# DBSession = sessionmaker(bind=engine)
+engine = create_engine('postgres://gsdyhfpzvhxvbz:3903f4b5f0dbed10fe05b20fd63cc77c0fc7a7001a3cf1625204628c4f982693@ec2-54-247-170-5.eu-west-1.compute.amazonaws.com:5432/ddvjg9iiu4vo5r')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 code = ''
--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from main import app, DBSession
+from main import app, db
 
-migrate = Migrate(app, DBSession)
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
